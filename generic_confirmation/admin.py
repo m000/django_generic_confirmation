d407e3198a0093679f496aecc7ef0a5f46dcb080
Manolis Stamatogiannakis <mstamat@gmail.com>
2017-02-27 13:17:35 +0100

    Import fix.

--- generic_confirmation/admin.py
+++ generic_confirmation/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import DeferredAction
+from .models import DeferredAction
 
 admin.site.register(DeferredAction, 
             list_display=('token', 'valid_until', 'confirmed', 'is_expired'))
