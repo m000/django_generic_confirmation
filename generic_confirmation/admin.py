a92d5ed06d0e47628c54d2fc6d398b0b81cd91d9
Manolis Stamatogiannakis <mstamat@gmail.com>
2017-02-28 17:16:08 +0100

    Show pickled object as read-only field in admin.

--- generic_confirmation/admin.py
+++ generic_confirmation/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import DeferredAction
 
-admin.site.register(DeferredAction, 
-            list_display=('token', 'valid_until', 'confirmed', 'is_expired'))
+class DeferredActionAdmin(admin.ModelAdmin):
+	list_display = ('token', 'valid_until', 'confirmed', 'is_expired')
+	readonly_fields=('form_input',)
+admin.site.register(DeferredAction, DeferredActionAdmin)
+
