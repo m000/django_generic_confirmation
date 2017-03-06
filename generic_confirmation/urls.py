215996a6c196de59891895a36969de1bffe7dfc7
Manolis Stamatogiannakis <mstamat@gmail.com>
2017-03-06 15:31:47 +0100

    Refactored views. Now GET and POST requests are handled by the same code. Object instance created is passed to the confirmation template.

--- generic_confirmation/urls.py
+++ generic_confirmation/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.confirm_by_form, {}, name="generic_confirmation_by_form"),
-    url(r'^(?P<token>\w+)/$', views.confirm_by_get, {}, name="generic_confirmation_by_get"),
+    url(r'^$', views.confirm, {'token': None},  name="generic_confirmation_by_form"),
+    url(r'^(?P<token>\w+)/$', views.confirm, {}, name="generic_confirmation_by_get"),
 ]
--- generic_confirmation/views.py
+++ generic_confirmation/views.py
@@ -3,13 +3,9 @@ from django.template import RequestContext
 from django.http import HttpResponseRedirect
 from .forms import ConfirmationForm
 
-
-
-def confirm_by_form(request, template_name='confirm.html',
-                    success_template_name='confirmed.html',
-                    success_url=None, success_message=None,
-                    form_class=ConfirmationForm):
-    """
+def confirm(request, token, template_name='confirm.html', success_template_name='confirmed.html',
+                    success_url=None, success_message=None, form_class=ConfirmationForm):
+    '''
     If ``success_url`` is not None a redirect to ``success_url`` will
     be issued, once the entered confirmation code was confirmed successfully.
     Optionally, of ``success_message`` is not None, it will be set via
@@ -18,42 +14,28 @@ def confirm_by_form(request, template_name='confirm.html',
     If ``success_url`` is None the template ``success_template_name`` will
     be rendered once the confirmation is complete. The ``success_message``
     will then be added to the template context instead of the message_set.
-
-    """
+    '''
     if request.method == 'POST':
+        # get token from POST
         form = form_class(request.POST)
-        if form.is_valid():
-            form.save()
-            if success_url is None:
-                return render_to_response(success_template_name, context=RequestContext(request, {
-                    'success_message': success_message
-                }))
-            else:
-                if success_message is not None and request.user.is_authenticated():
-                    request.user.message_set.create(message=success_message)
-                return HttpResponseRedirect(success_url)
+    elif token is not None:
+        # token has been parsed by urlconf
+        form = form_class({'token': token})
     else:
+        # no token provided - just render the form
         form = form_class()
-    return render_to_response(template_name, context=RequestContext(request, {'form': form}))
+        return render_to_response(template_name, context=RequestContext(request, {'form': form}))
 
-
-
-def confirm_by_get(request, token, template_name='confirm.html',
-                   success_template_name="confirmed.html",
-                   success_url=None, success_message=None,
-                   form_class=ConfirmationForm):
-    form = form_class({'token': token})
     if form.is_valid():
-        form.save()
+        instance = form.save()
         if success_url is None:
             return render_to_response(success_template_name, context=RequestContext(request, {
-                'success_message': success_message
+                'success_message': success_message,
+                'instance': instance,
             }))
         else:
             if success_message is not None and request.user.is_authenticated():
                 request.user.message_set.create(message=success_message)
             return HttpResponseRedirect(success_url)
     else:
-        return render_to_response(template_name, context=RequestContext(request, {}))
-
-
+        return render_to_response(template_name, context=RequestContext(request, {'form': form}))
